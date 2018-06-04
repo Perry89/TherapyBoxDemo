ea48d98367b57534aeb509e3e7a74581d552fef3
Perry89 <39899619+Perry89@users.noreply.github.com>
2018-06-04 08:24:53 +0100

    Inital

---
+++ TherapyBoxDemo/PageModels/SportsPageModel.cs
@@ -0,0 +1,91 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TherapyBoxDemo.PageModels
+{
+    public class SportsPageModel : FreshBasePageModel
+    {
+        System.IO.StreamReader reader;
+        public SportsPageModel()
+        {
+            GetCSVFile();
+        }
+        private async Task GetCSVFile()
+        {
+            var uri = new Uri(@"http://www.football-data.co.uk/mmz4281/1718/I1.csv");
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            WebResponse responseObject = await Task<WebResponse>.Factory.FromAsync(request.BeginGetResponse, request.EndGetResponse, request);
+            var responseStream = responseObject.GetResponseStream();
+            reader = new StreamReader(responseStream);
+            List<string> itemsList = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+
+                itemsList.Add(values[0]);
+                
+            }
+            itemsList.Contains("Juventus");
+            TeamList = itemsList;
+
+
+        }
+        private FreshAwaitCommand _searchCommand;
+        public FreshAwaitCommand SearchCommand
+        {
+            get
+            {
+                if (_searchCommand == null)
+                {
+                    _searchCommand = new FreshAwaitCommand(async (tcs) =>
+                    {
+                        await GetCSVFile();
+                        tcs.SetResult(true);
+                    });
+                }
+                return _searchCommand;
+            }
+
+        }
+        private List<string> _teamList;
+        public List<string> TeamList
+        {
+            get
+            {
+                return _teamList;
+            }
+            set
+            {
+                _teamList = value;
+                RaisePropertyChanged("TeamList");
+            }
+        }
+        private string _teamNameEntry;
+        public string TeamNameEntry
+        {
+            get
+            {
+                return _teamNameEntry;
+            }
+            set
+            {
+                _teamNameEntry = value;
+                RaisePropertyChanged("TeamNameEntry");
+            }
+        }
+        public class Teams
+        {
+            public string FirstTeam { get; set; }
+            public string SecondTeam { get; set; }
+            public string Result { get; set; }
+
+        }
+    }
+}
---
+++ TherapyBoxDemo/PageModels/TaskPageModel.cs
@@ -0,0 +1,43 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TherapyBoxDemo.PageModels
+{
+    public class TaskPageModel : FreshBasePageModel
+    {
+        public TaskPageModel()
+        {
+            var list = new List<TaskItems>();
+            list.Add(new TaskItems { TaskName = "Task1", TaskToggle = true });
+            list.Add(new TaskItems { TaskName = "Task2", TaskToggle = false });
+            list.Add(new TaskItems { TaskName = "Task3", TaskToggle = false });
+            list.Add(new TaskItems { TaskName = "Task4", TaskToggle = false });
+            list.Add(new TaskItems { TaskName = "Task5", TaskToggle = false });
+            list.Add(new TaskItems { TaskName = "Task6", TaskToggle = false });
+            list.Add(new TaskItems { TaskName = "Task7", TaskToggle = true });
+            list.Add(new TaskItems { TaskName = "Task8", TaskToggle = true });
+            TaskList = list;
+
+        }
+        private List<TaskItems> _taskList;
+        public List<TaskItems> TaskList
+        {
+            get
+            {
+                return _taskList;
+            }
+            set
+            {
+                _taskList = value;
+                RaisePropertyChanged("TaskList");
+            }
+        }
+    }
+    public class TaskItems
+    {
+        public string TaskName { get; set; }
+        public bool TaskToggle { get; set; }
+    }
+}
